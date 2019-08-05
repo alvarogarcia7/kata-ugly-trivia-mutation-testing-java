7349001050a2cb2bc4fae5e2185e226c8ce6f1f1
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2019-08-05 08:06:56 +0200

    Refactor: move purses array to Players

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -8,7 +8,6 @@ public class Game {
 	private Players playerList = new Players(this);
     ArrayList players = new ArrayList();
     int[] places = new int[6];
-    int[] purses  = new int[6];
     boolean[] inPenaltyBox  = new boolean[6];
     
     LinkedList popQuestions = new LinkedList();
--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Players.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Players.java
@@ -2,16 +2,18 @@ package com.adaptionsoft.games.uglytrivia;
 
 public class Players {
 	private Game game;
+	private int[] purses = new int[6];
+
 
 	public Players(Game game) {
 		this.game = game;
 	}
 
 	int currentPlayerCoins() {
-		return game.purses[game.currentPlayer];
+		return purses[game.currentPlayer];
 	}
 
 	int currentPlayerWinsACoin() {
-		return game.purses[game.currentPlayer]++;
+		return purses[game.currentPlayer]++;
 	}
 }
